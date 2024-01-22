fffad16a70dbf4b7a2c621a5c912f5618acc24b1
ercole89 <davide.ercole89@gmail.com>
2024-01-22 09:23:50 +0100

    lunedi

--- scripts/scriptIntermedi.ts
+++ scripts/scriptIntermedi.ts
@@ -32,38 +32,41 @@ async function main() {
 
 
     const contractParentAddresses: { [key: string]: string } = {
-        "Aria": "0xc4b77ba8F29c11D4e154B1aeb98FabDCcC6B614d",
-        "Luna": "0x2db533bDFba2af4e64A4B45d26cc4D6952b8eDCf",
-        "Ryker": "0x1d15E26eaC86fee67d2D78Ff9aFDe9dd74Cd148B",
-        "Thaddeus": "0x058CA62DC5A7FCef9f1D1C275601189572Ca699B"
+        "Aria": "0xAbca4357640539B169F6b16214AC3039F9f9aF38",
+        "Luna": "0x1E7219fAFf87265431320BB291845Ec308A03222",
+        "Ryker": "0x3b85e80d22a2778a56817dD8456d214c136D0f50",
+        "Thaddeus": "0x1447ADe5327463d0788f80A94F921a8b669F2B1c"
     };
+    
     
 
     const contractCatalogAddresses: { [key: string]: string } = {
-        "Aria": "0xB5272CC04a0C41b08b001bD04f7B6f50cAB527ca",
-        "Luna": "0xDe095f942f798f7F6fdCf4EE8bE34601e5b0c655",
-        "Ryker": "0x2a98a5d61b80f996402595FABfE3b47D54D9B1E7",
-        "Thaddeus": "0xF788baa8af703536a7a2Bf5Ba3307382CC35A5E0"
+        "Aria": "0xA9eE3A2A0B7e6490F493463E7F545EEd16b14e56",
+        "Luna": "0x840c13858090F2869Ba95B2c254Ae343aa1C10c6",
+        "Ryker": "0x80292f35D6F9EbF94A4911379c5019aDf76C4d33",
+        "Thaddeus": "0x375FA08398f793998371cC80774ab9f8CC531539"
     };
+    
 
     const contractItemAddresses: { [key: string]: string } = {
-        "AriaArmor": "0xe7C9a1088Be68b818A374D6286d9E5B6b6777AB0",
-        "AriaCap": "0x4494d76693A57d01b1Ab2CdCAC1fFF69618f1E95",
-        "AriaLeftHand": "0x2fc9D1a3bdc51aE8D7D041532851b42D200870a2",
-        "AriaRightHand": "0xa14e50831f03bE4888785a48De6D9Ee9281C51cA",
-        "LunaArmor": "0x2300705f9592983d3e1141A822514F4Af92E0caC",
-        "LunaCap": "0x54802Fc3678559Ef2aaF2726083Cfaa5F8284262",
-        "LunaLeftHand": "0xBE0d77DD8D3FDF4d2d4C147dCd6ce8D12d1aed7B",
-        "LunaRightHand": "0x2ED07cfEC002dE223018E901EA5ED3C5d8e33D92",
-        "RykerArmor": "0x205D08f8bDe1926b3B800cABCC2e0523CBd356B9",
-        "RykerCap": "0x3F57809Fcf34B793e17Cf7e0c98918B67B3599f0",
-        "RykerLeftHand": "0x71997A39627017Bf83B09fF90Ba45242241Fbe05",
-        "RykerRightHand": "0x06Fe54F5D1Cde870ED1D7Ea1fc67a2142BBEB89f",
-        "ThaddeusArmor": "0x7c9e964fE85b70be6EEb61d58440934F4adB5Bd2",
-        "ThaddeusCap": "0xA919815976edC4f9F36fD025F162D05a5C70D66a",
-        "ThaddeusLeftHand": "0x0648b83950F637Da2e5499ADCEBe88B5E4FB1225",
-        "ThaddeusRightHand": "0xb50d0D9c39ebdE6D914A80C05E2E5004beaCCEE8"
+        "AriaArmor": "0x371Fef6D54FEa1ECf125F6633d6F61055cbF45F4",
+        "AriaCap": "0x5681fEa9f7219CEaD0e087C80F9ab539F3d3553C",
+        "AriaLeftHand": "0x86c5A636C45bacFc736A2E5a142C47FC7A4EA812",
+        "AriaRightHand": "0x2ec208A0fD1BdF6a7db292522cac95Cba674E000",
+        "LunaArmor": "0xcE8401Df8696151a290777d4547D33F95a7458bD",
+        "LunaCap": "0xDb163CE2f621e4DE80dC62bF41e78fA68c3b4f9c",
+        "LunaLeftHand": "0x65a1B700E89824eB5238fFBaf70Aec2A7D540f1C",
+        "LunaRightHand": "0x1FD9A1e512348D8E7f70F3f9a8d15D2F0eeb11bb",
+        "RykerArmor": "0xAB5aAF41Aaa1b4311218cF8A3C2b80b5c008429D",
+        "RykerCap": "0x7cDd1baC0E265B08090c928CFc7a079F50e7241c",
+        "RykerLeftHand": "0xdDC8705E20819b34156122a8B08764029f131609",
+        "RykerRightHand": "0xc13684aec8EE62ec58092bdc27036c233db23789",
+        "ThaddeusArmor": "0x0847f79Ad21345642d0C10dF1714f7Fe442a49F0",
+        "ThaddeusCap": "0xa13F3BE3cc14F0c54e4B2504f9B4C52802Cb838C",
+        "ThaddeusLeftHand": "0xb174E09f5F83f0bcc9b50bB89632b02FCfCCF00D",
+        "ThaddeusRightHand": "0xF0Bb07B3886625eC1AD7b16b7A0a9F15e4C31b1d"
     };
+    
     
 
 
