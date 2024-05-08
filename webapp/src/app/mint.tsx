dce33ebbd602f00ebcc6d91fa0698dc271e7ebdd
ercole89 <davide.ercole89@gmail.com>
2024-05-08 11:10:00 +0200

    simpleMint frontEndPage

--- webapp/src/app/mint.tsx
+++ webapp/src/app/mint.tsx
@@ -1,11 +1,17 @@
+'use client'
+
 import { useState } from 'react';
 import { Button } from "@nextui-org/react";
 import { ethers } from "ethers";
-
+import {
+  TimeSquadAria,
+} from '../../../typechain-types';
+import TimeSquadAriaABI from '../../../artifacts/contracts/parent/TimeSquadAria.sol/TimeSquadAria.json';
+import ItemsAriaCapABI from '../../../artifacts/contracts/child/itemsAriaCap.sol/itemsAriaCap.json';
 
 const MintButton = () => {
   const [isMinting, setIsMinting] = useState(false);
-
+  const BASE_IPFS_URI = 'ipfs://Qmcq4NvF15Jf9VyEi1yDHyoAKqQdok5NdZ2PJJcwn6gW9B';  //metadata ipfs
   const mintNFT = async () => {
     if (typeof window.ethereum === 'undefined') {
       alert('MetaMask is not installed!');
@@ -15,15 +21,22 @@ const MintButton = () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
 
-    // Supponiamo di avere un contratto chiamato TimeSquadAria con l'indirizzo salvato in .env
-    const timeSquadAriaAddress = process.env.NEXT_PUBLIC_TIMESQUAD_ARIA_ADDRESS;
-    const timeSquadAriaABI = []; // Inserisci l'ABI del tuo contratto qui
+    const timeSquadAriaAddress = '0x443a2b92bcfdcfbe64a9c34f88cdbfbb63da4610';
+    const timeSquadAriaABI = TimeSquadAriaABI.abi;  // Utilizzo dell'ABI importato dal typechain
     const timeSquadAria = new ethers.Contract(timeSquadAriaAddress, timeSquadAriaABI, signer);
+
+    // Indirizzo e ABI del contratto ItemsAriaCap
+    const itemsAriaCapAddress = "0x0c6cb5e5568ef916f220f9a448ee66ae3f10f82a";
+    const itemsAriaCapABI = ItemsAriaCapABI.abi;  // Utilizzo dell'ABI importato dal typechain
+    const itemsAriaCap = new ethers.Contract(itemsAriaCapAddress, itemsAriaCapABI, signer); // Presumendo che TimeSquadAria e ItemsAriaCap abbiano la stessa ABI
 
     try {
       setIsMinting(true);
-      const tx = await timeSquadAria.mintWithEquippableAsset(
-        // parametri necessari qui
+      const tx = await itemsAriaCap.nestMintWithAssets(
+        timeSquadAriaAddress, // A chi inviare
+        1, // destinationId del primo NFT parent
+        `${BASE_IPFS_URI}/items/aria/02_cap_primary_asset.json`, // TokenURI del NFT figlio
+        [1, 2] // Assets IDs
       );
       await tx.wait();
       alert('Minting successful!');
--- webapp/src/app/page.tsx
+++ webapp/src/app/page.tsx
@@ -1,13 +1,13 @@
 import Image from 'next/image'
 import NavBar from './navbar'
 import { Button} from "@nextui-org/react";
+import MintButton from './mint';  // Assicurati che il percorso sia corretto
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <NavBar />
-      <Button>
-qui cosa metto?
-      </Button>
+      <MintButton/>
 
     </main>
   )
